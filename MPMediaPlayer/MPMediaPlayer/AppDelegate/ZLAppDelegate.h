4aa00f9bc97f3f34081233cb4a3c88f7b72ab6b1
zhiliang <zhiliang729@163.com>
2013-06-07 13:15:36 +0800

    Initial commit

---
+++ MPMediaPlayer/MPMediaPlayer/AppDelegate/ZLAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  ZLAppDelegate.h
+//  MPMediaPlayer
+//
+//  Created by zhangliang on 13-6-7.
+//  Copyright (c) 2013年 www.fetchview.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ZLViewController;
+
+@interface ZLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ZLViewController *viewController;
+
+@end
---
+++ MPMediaPlayer/MPMediaPlayer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MPMediaPlayer
+//
+//  Created by zhangliang on 13-6-7.
+//  Copyright (c) 2013年 www.fetchview.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZLAppDelegate class]));
+    }
+}
